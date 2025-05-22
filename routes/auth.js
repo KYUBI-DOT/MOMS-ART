e409c378dee204f03168040794910dce80af7f93
KYUBI-DOT <133933242+KYUBI-DOT@users.noreply.github.com>
2025-05-22 18:36:52 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,22 @@
-const express=require("express");
+const express = require("express");
 const router = express.Router();
-const authController=require('../controllers/auth')
+const authController = require("../controllers/auth");
 
-router.post('/register',authController.register)
+//  Registration
+router.post("/register", authController.register);
+
+//  Login
 router.post("/login", authController.loginUser);
 
+//  Forgot Password - Request Reset
+router.get("/forgot-password", (req, res) => {
+  res.render("forgot-password", { message: null });
+});
 
-module.exports=router;
+router.post("/forgot-password", authController.sendResetLink);
+
+//  Password Reset - With Token
+router.get("/password-reset/:token", authController.renderResetForm);
+router.post("/password-reset/:token", authController.resetPassword);
+
+module.exports = router;
